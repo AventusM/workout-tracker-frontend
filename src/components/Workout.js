1afc82c1e8350ebcd65eaca554fb428517dec309
Anton <anton.moroz@helsinki.fi>
2019-07-11 17:13:55 +0300

    Form for creating workouts (with multiple results) done with Formik

--- src/components/Workout.js
+++ src/components/Workout.js
@@ -1,24 +1,87 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { createWorkout, fetchData } from '../reducers/workouts'
-import { Formik, FieldArray, Form } from 'formik'
+import { Formik, FieldArray, Form, Field } from 'formik'
 
 
-// 2 parts
-// 1st -- add items (results) to be submitted later
-// 2nd -- submit the whole thing
+// TODO -- CLEAR VALUES ON SUBMIT
+// TODO -- CLEAR VALUES ON SUBMIT
+// TODO -- CLEAR VALUES ON SUBMIT
 const WorkoutForm = () => {
   const dispatch = useDispatch()
 
   return (
-    <Formik initialValues={{ results: [] }} onSubmit={() => { }}>
-      {({ values }) => (
+    <Formik initialValues={{ results: [{ name: 'Bench press', type: 'Barbell', weight: 0, repetitions: 0, sets: 0 }] }}
+      onSubmit={(values) => { 
+        dispatch(createWorkout({results: values.results})) 
+        }}>
+
+      {({ values, handleChange }) => (
         <Form>
-          <input />
           <FieldArray name="results">{({ push }) => (
-            <div>
-              <button type="button" onClick={() => push()}>add to list</button>
-            </div>
+            <Fragment>
+              {values.results.map((result, index) => {
+                return (
+                  <Fragment key={index}>
+                    <div>
+                      <label>Choose discipline</label>
+                      <Field
+                        component="select"
+                        name={`results[${index}].name`}
+                        value={result.name}
+                        onChange={handleChange}>
+                        <option disabled selected label=" ">-- select an option --</option>
+                        <option value="Bench press">Bench press</option>
+                        <option value="Military press">Military press</option>
+                        <option value="Deadlift">Deadlift</option>
+                        <option value="Squat">Squat</option>
+                      </Field>
+                    </div>
+
+                    <div>
+                      <label>Equipment type</label>
+                      <Field
+                        component="select"
+                        name={`results[${index}].type`}
+                        value={result.type}
+                        onChange={handleChange}>
+                        <option disabled selected label=" ">-- select an option --</option>
+                        <option value="Barbell">Barbell</option>
+                        <option value="Dumbbell">Dumbbell</option>
+                        <option value="Machine">Machine</option>
+                      </Field>
+                    </div>
+
+                    <div>
+                      <label>Weight (kg)</label>
+                      <Field
+                        name={`results[${index}].weight`}
+                        value={result.weight}
+                        onChange={handleChange} />
+                    </div>
+
+                    <div>
+                      <label>Reps</label>
+                      <Field
+                        name={`results[${index}].repetitions`}
+                        value={result.repetitions}
+                        onChange={handleChange} />
+                    </div>
+
+                    <div>
+                      <label>Sets</label>
+                      <Field
+                        name={`results[${index}].sets`}
+                        value={result.sets}
+                        onChange={handleChange} />
+                    </div>
+                  </Fragment>
+                )
+              })}
+
+              {/* Push requires default values i guess... */}
+              <button type="button" onClick={() => push({ name: 'Bench press', type: 'Barbell', weight: 0, repetitions: 0, sets: 0 })}>add to list</button>
+            </Fragment>
           )}
           </FieldArray>
           <button type="submit">submit</button>
